c858978cde382018fc055c9845fce8fb37bb202e
Sachin Mahajan <sachinm78@gmail.com>
2019-03-10 22:44:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,10 +9,10 @@ end
 
 
 def reverse_each_word(string)
-  array = string.split(" ")
-  test_array = []
-  array.collect do|string|
-    test_array << string.reverse 
+  array = string.split (" ")
+  new_array = []
+  array.collect do |string|
+    new_array << string.reverse
   end
-  test_array.join(" ")
+  new_array.join (" ")
 end
